511c232f90c3a9f76eac8bd21eb892f0f16d66bc
Dana <martinelli@gmail.com>
2025-01-21 20:31:03 -0500

    dsa

--- src/components/NodeDetailsBar.vue
+++ src/components/NodeDetailsBar.vue
@@ -20,15 +20,9 @@
       <div class="flex items-center space-x-2">
         <button
           @click="saveNode"
-          class="h-6 px-3 text-xs font-medium text-white bg-blue-600 rounded hover:bg-blue-700 focus:ring-2 focus:ring-blue-500"
+          class="h-6 px-2 text-green-300 bg-black/50 rounded hover:bg-black/20 focus:ring-2 focus:ring-black/50"
         >
-          Save
-        </button>
-        <button
-          @click="closePanel"
-          class="h-6 px-3 text-xs font-medium text-gray-300 bg-gray-700 rounded hover:bg-gray-600 focus:ring-2 focus:ring-gray-500"
-        >
-          Close
+          <CheckCircleIcon class="w-4 h-4" />
         </button>
       </div>
     </div>
@@ -39,6 +33,7 @@
 import { ref, watch } from "vue";
 import { dbService } from "@/services/dbService";
 import { useToast } from "vue-toastification";
+import { CheckCircleIcon } from "@heroicons/vue/24/outline";
 
 // Initialize toast
 const toast = useToast();
